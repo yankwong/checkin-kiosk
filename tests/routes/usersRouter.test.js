2ab6ecbf73859a5e91e7a9a146d51f9a026b260b
Yan Kwong <yantkwong@gmail.com>
2018-09-26 13:01:43 -0700

    initial setup for express and basic tests

---
+++ tests/routes/usersRouter.test.js
@@ -0,0 +1,19 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const checkInRouter = require('../../routes/checkInRouter');
+
+describe('CheckIn routes tests', () => {
+
+    context('addCheckin', () => {
+        it('should add a checkin');
+    });
+
+    context('deleteCheckin', () => {
+        it('should delete a checkin');
+    });
+
+    context('editCheckin', () => {
+        it('should edit a checkin');
+    });
+});
