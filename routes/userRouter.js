a04cdc1edc9bd46a718c2339b1eb3a37714431ff
Yan Kwong <yantkwong@gmail.com>
2018-10-02 08:42:28 -0700

    init routers and unit tests template (WIP)

---
+++ routes/userRouter.js
@@ -0,0 +1,23 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const userRouter = require('../../routes/userRouter');
+
+describe('User routes tests', () => {
+    
+    context('register', () => {
+        it('should ');
+    });
+    
+    context('login', () => {
+        it('should ');
+    });
+
+    context('logout', () => {
+        it('should ');
+    });
+
+    context('delete', ()=> {
+
+    });
+});
